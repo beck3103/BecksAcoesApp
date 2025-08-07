9f3e47e9f12a9e332df7d99a01485bea235ff344
Marlon Beck <marlon.beck99@hotmail.com>
2025-08-07 20:57:41 -0300

    Adding new page showing results

---
+++ BecksAcoesApp/Converters/ApplicationDtoToViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BeckAcoesApp.Application.Dtos;
+using BecksAcoesApp.ViewModels;
+
+namespace BecksAcoesApp.Converters;
+
+internal static class ApplicationDtoToViewModel
+{
+    internal static FundamentusDetailsViewModel ToFundamentusDetailsViewModel(this FundamentusDto fundamentusDto)
+    { 
+        return new FundamentusDetailsViewModel
+        {
+            Papel = fundamentusDto.Papel,
+            CotacaoAtual = decimal.TryParse(fundamentusDto.Cotacao, out var cotacao) ? cotacao : 0,
+            DividendYield = fundamentusDto.DivYield
+        };
+    }
+}
