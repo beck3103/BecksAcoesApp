1c5e0ce28bc26f75605b194a603764ab8d0d8414
Marlon Beck <marlon.beck99@hotmail.com>
2025-08-20 20:24:57 -0300

    Adding maximum price and Fair price calculation

--- Application/Interfaces/Services/IFundamentusAppService.cs
+++ Application/Interfaces/Services/IFundamentusAppService.cs
@@ -5,4 +5,8 @@ namespace BeckAcoesApp.Application.Interfaces.Services;
 public interface IFundamentusAppService
 {
     Task<FundamentusDto?> GetFundamentusDataAsync(string ticket);
+
+    decimal CalcularPrecoJusto(string precoSobreLucro, string crescimentoReceitaUltimosCincoAnos, string cotacao);
+
+    decimal CalcularPrecoTeto(string cotacao, string dividendYield);
 }
